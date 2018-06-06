c578cb65535977d0a52a29a41722728ea34b0013
sdrudz <pedropauloroachy@gmail.com>
2018-06-06 20:54:30 -0300

    layout site e backoffice

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   namespace :site do
-  get 'home/index'
+  get 'home', to:'home#index'
   end
+
+  get 'backoffice', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
-  get 'dashboard/index'
+  get 'dashboard', to: 'dashboard#index'
   end
-
+  
   devise_for :admins
   devise_for :members
   
